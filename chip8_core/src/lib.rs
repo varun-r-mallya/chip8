7e6ae72fe25c38cd7bc9c1a68e429f351778d73a
varun-r-mallya <varunrmallya@gmail.com>
2024-12-04 04:45:33 +0530

    init and completed till pg 19

---
+++ chip8_core/src/lib.rs
@@ -0,0 +1,127 @@
+const RAM_SIZE: usize = 4096;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+const START_ADDR: u16 = 0x200;
+const NUM_KEYS: usize = 16;
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+pub struct Emu {
+    pc: u16,                                      //program counter
+    ram: [u8; RAM_SIZE],                          //memory
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT], //screen size
+    v_reg: [u8; NUM_REGS],                        //v - registers
+    i_reg: u16,                                   //i - register
+    sp: u16,                                      // stack pointer
+    stack: [u16; STACK_SIZE],                     // stack
+    keys: [bool; NUM_KEYS],                       // key presses on the chip8
+    dt: u8,                                       // delay timer
+    st: u8,                                       // sound timer
+}
+
+impl Emu {
+    pub fn new() -> Self {
+        let mut new_emu = Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_REGS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emu
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0; RAM_SIZE];
+        self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    fn push(&mut self, val: u16) {
+        //push method for stack
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    // game code error when empty stack popped, so not handling the complete panic.
+    fn pop(&mut self) -> u16 {
+        //pop method for stack
+        self.sp -= 1;
+        self.stack[self.sp as usize]
+    }
+
+    pub fn tick(&mut self) {
+        //Fetch
+        let op = self.fetch();
+        //decode and execute
+        self.execute(op);
+    }
+
+    fn execute(&mut self, op: u16) {
+        //digit separation
+        let digit1 = (op & 0xF000) >> 12;
+        let digit2 = (op & 0x0F00) >> 8;
+        let digit3 = (op & 0x00F0) >> 4;
+        let digit4 = op & 0x000F;
+        //TODO: continue from page 19
+        match (digit1, digit2, digit3, digit4) {
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
+        }
+    }
+
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc + 1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte; //convert to Big Endian
+        self.pc += 2; //move ahead
+        op
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
+        }
+        if self.st > 0 {
+            if self.st == 0 {
+                //BEEP after this ( implement later )
+            }
+            self.st -= 1;
+        }
+    }
+}
