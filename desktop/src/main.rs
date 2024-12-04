fbb0b02875822929d2dde8602928cf8e623deca8
varun-r-mallya <varunrmallya@gmail.com>
2024-12-04 22:33:48 +0530

    working sim

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,10 +1,37 @@
 use chip8_core::*;
-use sdl2::event::Event;
 use std::env;
+use std::fs::File;
+use std::io::Read;
+
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
+    //clear to black
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let screen_buf = emu.get_display();
+    //set draw color to white and iterate through each point
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            //1d array to 2d positions
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+    canvas.present();
+}
 
 fn main() {
     //argument handling
@@ -13,6 +40,14 @@ fn main() {
         println!("Usage: cargo run path/to/game");
         return;
     }
+
+    let mut chip8 = Emu::new();
+
+    //File opening logic
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer = Vec::new();
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
 
     // Setup SDL
     let sdl_context = sdl2::init().unwrap();
@@ -37,5 +72,7 @@ fn main() {
                 _ => (),
             }
         }
+        draw_screen(&chip8, &mut canvas);
+        chip8.tick();
     }
 }
